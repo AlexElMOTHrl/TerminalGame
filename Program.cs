ae10f2b625a7fc68268b62cda72960af365c124a
AlexElMOTHrl <vivamoth93@gmail.com>
2022-11-11 14:38:03 +0100

    Upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,30 @@
 ﻿using AlexLibs;
 using System.Dynamic;
+using System.Text;
+using System.Threading;
 
+
+namespace JuegoTerminal;
 internal class Program
 {
-    
+
     private static void Main(string[] args)
     {
         // Var
+    #region Variables
         
         var defaultConsoleColor = Console.BackgroundColor;
         var defaultConsoleTextColor = Console.ForegroundColor;
 
+    #endregion
         // ExpandoObjects
-
+    #region Items
         dynamic items = new ExpandoObject();
-        items.DescribeItem = (Func<String>)(() => {
-            return $"Id: {items.Name}:\nNombre: {items.DisplayName}\nDescripción: {items.Description}\n";
+        items.DescribeItem = (Func<String>)(() =>
+        { //! NO FUNCIONA, INTENTA CREAR UN EXPANDO PARA PEDIR LAS VARIABLES DEL OBJETO SELECCIONADO.
+            return $"Id: {items}:\nNombre: {items.descItem.DisplayName}\nDescripción: {items.descItem.Description}\n";
         });
-
+    
         items.caca = new ExpandoObject();
         items.caca.Id = 5;
         items.caca.DisplayName = "Caca";
@@ -28,16 +35,17 @@ internal class Program
         items.colacao.Id = 4;
         items.colacao.DisplayName = "Colacao";
         items.colacao.CodeName = "colacao";
-        items.colacao.Description = "Esto es un colacao, te lo puedes beber, ¿Tengo que explicarlo de verdad?";        
+        items.colacao.Description = "Esto es un colacao, te lo puedes beber, ¿Tengo que explicarlo de verdad?";
 
         items.testitem = new ExpandoObject();
         items.testitem.Id = 999;
         items.testitem.DisplayName = "TEST ITEM";
         items.testitem.CodeName = "testitem";
-        items.testitem.Description = "DEBUG - OBJETO DE PRUEBA - ¿FUNCIONO?";        
-
+        items.testitem.Description = "DEBUG - OBJETO DE PRUEBA - ¿FUNCIONO?";
+    #endregion
         // Menus y parecidos.
-
+    #region Menus
+        
         string errorScreen = @$"
 #################################################################################
 #####                           ERROR SERIO                                ######
@@ -55,6 +63,7 @@ ERROR => ";
 
 - Selección: ";
 
+    #endregion
         //string itemDesc = @"Nombre: {describedItem}";
 
         // Fin de "Menus y parecidos".
@@ -110,7 +119,7 @@ ERROR => ";
         void ItemDesc()
         {
             Console.WriteLine("Item para recivir la informacion:");
-            Console.WriteLine($"DEBUG:\nId: {items.colacao.Id}\nNombre: {items.colacao.DisplayName}\nDescripcion: {items.colacao.Description}\nDEBUG 2 --------\n{items.DescribeItem}");
+            Console.WriteLine($"DEBUG:\nId: {items.colacao.Id}\nNombre: {items.colacao.DisplayName}\nDescripcion: {items.colacao.Description}\nDEBUG 2 --------\n{items.DescribeItem()}");
             int itemSelec = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
             Console.Write("DEBUG?"); // DEBUG DE CLASES
@@ -148,8 +157,8 @@ ERROR => ";
                         break;
 
                     default:
-                    DebugERROR("Fuera de rango...");
-                    KeyToContinue();
+                        DebugERROR("Fuera de rango...");
+                        KeyToContinue();
                         break;
                 }
             }
